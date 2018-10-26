a5fb6e5b11ee3dc2185b13c28a63d5b8e5105f4a
giordifungula <giordifungula@gmail.com>
2018-10-26 11:21:37 +0200

    working on the function

---
+++ main.js
@@ -0,0 +1,14 @@
+function makeNegative (number) {
+
+    // write the code for your function here
+    if( number <= 0) {
+        number;
+    }else {
+        number * -1;
+        // if the number is positve make it negative
+    }
+    return number;
+  }
+  
+  // call your function here
+  makeNegative(5);
